a5be9c671851b872d2ec53559d79ccb37e91269a
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-10-24 15:05:52 +0300

    Improve ListEntry, drop lazy, move api to pointer

--- Sources/ListEntry/ListEntry+Collection.swift
+++ Sources/ListEntry/ListEntry+Collection.swift
@@ -1,15 +1,11 @@
-import Foundation
-
 extension ListEntry: BidirectionalCollection {
     public typealias Index = UnsafeMutablePointer<ListEntry<T>>
     public var startIndex: Index {
-        var head = self
-        return head.next
+        return pointer.next
     }
 
     public var endIndex: Index {
-        var head = self
-        return head.originalPointer
+        return pointer
     }
 
     public subscript(position: Index) -> UnsafeMutablePointer<ListEntry<T>> {
