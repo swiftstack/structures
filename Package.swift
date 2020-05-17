0c55946a643576556abc0e2f10a4dc6dbfac07da
Tony Freeman <tonyfreeman@users.noreply.github.com>
2020-05-17 22:06:32 +0300

    [Package] Rename to Structures

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "LinkedList",
+    name: "Structures",
     products: [
         .library(name: "LinkedList", targets: ["ListEntry"])
     ],
